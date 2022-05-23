0ca514e9856cf0de7691158b6f20e02b30d90e4d
Treeuu <96989754+Treeuu@users.noreply.github.com>
2022-05-23 21:46:57 +0300

    Add files via upload

---
+++ perk.py
@@ -0,0 +1,27 @@
+print("                                         version 0.1                                 ")
+print('/////////////////////////////////////////////////////////////////////////////////////')
+lesson =  str(input("Enter what you want: "))
+
+if lesson == "calculater":
+    print('              Calculater           ')
+print('///////////////////////////////////')
+print("/           version:0.1           /")
+print("/                                 /")
+print("/                                 /")
+print("/                                 /")
+print("/                                 /")
+number1= int(input("number1 - "))
+number2= int(input("number1 - "))
+print("/                                 /")
+res1 = number1 + number2
+res2 = number1 * number2
+res3 = number1 / number2
+print("/                                 /")
+print("/ result =", res1, res2, res3,   "/")
+print('///////////////////////////////////')
+
+if lesson == "gaben":
+    print("did you want a trick")
+
+
+    
