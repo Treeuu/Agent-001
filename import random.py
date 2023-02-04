a1c1d9d487647ee7680d62865f48cd134ece886a
Treeuu <96989754+Treeuu@users.noreply.github.com>
2023-02-04 16:10:40 +0200

    Add files via upload

---
+++ import random.py
@@ -0,0 +1,12 @@
+name = input("user name:" )
+if name == "bob":
+    print ("your in ", name)
+    num_1 = float(input( "enter number one: "))
+    num_2 = float(input( "enter number two: "))
+    res =  float(int(num_1)) + float(int(num_2))
+    res_2 = num_1 * num_2
+    res_3 = num_1 / num_2
+    print("your result is: ", res, res_2, res_3)
+elif name == 'rip':
+    for i in range(1, 11):
+        print('#'*i)
