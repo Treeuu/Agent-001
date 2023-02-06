a1ebb1a7bd4434930ed9682cba8c1ed76ebca09a
Treeuu <96989754+Treeuu@users.noreply.github.com>
2023-02-06 16:58:42 +0200

    Update import random.py

--- import random.py
+++ import random.py
@@ -1,12 +1,17 @@
 name = input("user name:" )
 if name == "bob":
     print ("your in ", name)
-    num_1 = float(input( "enter number one: "))
-    num_2 = float(input( "enter number two: "))
-    res =  float(int(num_1)) + float(int(num_2))
-    res_2 = num_1 * num_2
-    res_3 = num_1 / num_2
-    print("your result is: ", res, res_2, res_3)
+num_1 = float(input("enter first number: "))
+aqua = (input("enter symbol "))
+num_2 = float(input("enter second number "))
+if aqua == '+':
+    print(num_1 + num_2)
+elif aqua == '-':
+    print(num_1 - num_2)
+elif aqua == '*':
+    print(num_1 * num_2)
+elif aqua == '/':
+    print(num_1 / num_2)
 elif name == 'rip':
     for i in range(1, 11):
         print('#'*i)
@@ -15,3 +20,4 @@ elif name == 'lis':
     print (a)
 elif name == "byte_sis":
     print(name.__sizeof__())
+
